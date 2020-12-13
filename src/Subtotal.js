437fa8a185f74e2609f8d9e6be65f391b6c490f4
Deep310 <dparekh2019@gmail.com>
2020-12-13 00:53:16 -0500

    added checkout items functionality using React Context API

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import CurrencyFormat from 'react-currency-format';
+import { useStateValue } from './StateProvider';
 import './Subtotal.css';
 
 function Subtotal() {
+	const [{ basket }, dispatch] = useStateValue();
+
+	let value = 0;
+	var i;
+	for (i = 0; i < basket.length; i++) {
+		value = value + basket[i].price;
+	}
+
 	return (
 		<div className="subtotal">
 			<CurrencyFormat
 				renderText={(value) => (
 					<>
 						<p>
-							Subtotal (0 items): <strong>0</strong>
+							Subtotal ({basket?.length} items): <strong>{value}</strong>
 						</p>
 						<small className="subtotal__gift">
 							<input type="checkbox" />
@@ -18,7 +27,7 @@ function Subtotal() {
 					</>
 				)}
 				decimalScale={2}
-				value={0}
+				value={value}
 				displayType={'text'}
 				thousandSeparator={true}
 				prefix={'$'}
