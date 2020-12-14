135038c3ce15a4b3fec722f2d953002352b7f67b
Deep310 <dparekh2019@gmail.com>
2020-12-13 21:50:23 -0500

    added dummy text when checkout caart is empty

--- src/Checkout.js
+++ src/Checkout.js
@@ -40,6 +40,18 @@ function Checkout() {
 									<Link to="/">
 										<p>Shop items</p>
 									</Link>
+									<div className="checkout__buttons">
+										<Link to="/login">
+											<button className="checkout__loginButton">
+												Sign in to your account
+											</button>
+										</Link>
+										<Link to="/register">
+											<button className="checkout__registerButton">
+												Sign up now
+											</button>
+										</Link>
+									</div>
 								</div>
 							</div>
 						)}
